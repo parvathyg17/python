e263074a0a1ac67874f9cff0962ebebcda7d1b77
parvathyg17 <parvathyg05@gmail.com>
2023-08-24 16:38:27 +0530

    gjg

--- guidelete.py
+++ guidelete.py
@@ -1,3 +1,10 @@
-from tkinter import *
-w=Tk()
-
+import tkinter as tk
+def destroywindow():
+    root.destroy()
+root=tk.Tk()
+root.title("Destroy window example")
+label=tk.Label(root,text="kukkumon",font=('Times New Roman','80'),fg="black",bg="white")
+label.pack(padx=20,pady=20)
+destroy_button=tk.Button(root,text="Close",command=destroywindow)
+destroy_button.pack()
+root.mainloop()
